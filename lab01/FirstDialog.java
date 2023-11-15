bbd53ca326c081079e8b1542d1d3fec4e19b5644
Trung Nghĩa Trần <nghia.tt213581@sis.hust.edu.vn>
2023-11-15 18:28:33 +0700

    delete old files

--- lab01/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-package Lab01;
-import javax.swing.*;
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hello world! How old are you? ");
-        System.exit(0);
-    }
-}
